508cfa8de96503356baed1a6019b6df2f2591cee
Marcelo Gusmão Gumiero <mgumiero9@gmail.com>
2020-10-31 12:43:33 -0300

    saving work

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 function combos(word) {
-	let o = {};
+	let obj = {};
 	let arr = [];
 	let fixedChars = [];
-	let str = word;
+	let str = [...word];
 	let varChars;
+	let objOrdinary = 0;
 	let step = 1;
 	let charPos = 0;
 	(function iterator() {
 		arr = [];
 		if (step <= word.length && charPos < str.length) {
 			if (step === 1) {
-				[...str].forEach(c => arr.push(c));
+				// str.forEach(c => arr.push(c));
+				str.forEach(c => {
+					obj[objOrdinary] = c;
+					objOrdinary++;
+				});
+				charPos++
 			} else {
-				fixedChars = [...str].splice(charPos, step - 1);
-				[...str].forEach(c => {
+				fixedChars = str.splice(charPos, step - 1);
+				str.forEach(c => {
 					arr.push(fixedChars[0], c);
 				});
+				obj[objOrdinary] = arr;
+				objOrdinary++;
 				charPos++;
+				step++;
 			}
-			o[step] = arr;
-			step++;
 			iterator();
 		}
 	})();
-	Object.keys(o).forEach(key => console.log((o[key].join(''))));
+	Object.keys(obj).forEach(key => {
+		if (obj[key] instanceof Array) {
+			console.log((obj[key].join('')));
+		} else {
+			console.log(obj[key]);
+		}
+	});
 }
 combos('abc')
 // combos('abcd')
